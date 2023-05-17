e768da89de28546c0a4e1e65453bdeb727411ee8
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-05-17 20:47:05 +0530

    def pali(n):     s=n     m=0     while s:         re=s%10         m=m*10+re         s=s//10     return n==m
    n=int(input())
    np=n+1
    pp=n-1
    while np:
        if pali(np):
            break
        np+=1
    while pp:
        if pali(pp):
            break
        pp-=1
    if np-n < n-pp:
        print(np)
    elif np-n == n-pp:
        print(pp,np)
    else:
        print(pp)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,30 +1,26 @@
 def pali(n):
-   s=n
-   v=0
-   while s:
-       re=s%10
-       v=v*10+re
-       s=s//10
-   return v==n
-
+    s=n
+    m=0
+    while s:
+        re=s%10
+        m=m*10+re
+        s=s//10
+    return n==m
+    
 n=int(input())
-s=n+1
-while s:
-    if pali(s):
-        l=s
-        break;
-    s+=1
-m=n-1
-while m:
-    if pali(m):
-        r=m
-        break;
-    m-=1
-if l-n < n-r:
-    print(l)
-elif l-n == n-r:
-    print(r,l)
+np=n+1
+pp=n-1
+while np:
+    if pali(np):
+        break
+    np+=1
+while pp:
+    if pali(pp):
+        break
+    pp-=1
+if np-n < n-pp:
+    print(np)
+elif np-n == n-pp:
+    print(pp,np)
 else:
-    print(r)
-    
-    
+    print(pp)
