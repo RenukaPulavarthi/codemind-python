19e637b4eec180a78cee9458e6699250a17d55b9
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-05-19 21:48:11 +0530

    def fac(n):     s=0     for i in range(1,n):         if n%i==0:             s+=i     return s
    n=int(input())
    m=int(input())
    if n==fac(m) and m==fac(n):
        print("Amicable")
    else:
        print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,12 +1,13 @@
-def fac(n:int)->int:
+def fac(n):
     s=0
     for i in range(1,n):
         if n%i==0:
-            s=s+i
-    return s;
-a=int(input())
-b=int(input())
-if fac(a)==b and fac(b)==a:
+            s+=i
+    return s
+    
+n=int(input())
+m=int(input())
+if n==fac(m) and m==fac(n):
     print("Amicable")
 else:
     print("Not Amicable")
