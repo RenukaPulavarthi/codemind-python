9536eec0f8a4650095f112e68f4f42ba7dccce2b
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-05-31 13:37:29 +0530

    n=input() lst=[] for i in n:     lst.append(int(i)) for i in range(len(lst)):     if lst[i]==6:         lst[i]=9         break; for i in lst:     print(i,end="")

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,17 +1,10 @@
-n=int(input())
-l=n%10;
-k=(n//10)%10
-m=(n//100)%10
-o=n//1000
-if o==6:
-    o=9
-elif m==6:
-    m=9
-elif k==6:
-    k=9
-elif l==6:
-    l=9
-print(o,end="")
-print(m,end="")
-print(k,end="")
-print(l,end="")
+n=input()
+lst=[]
+for i in n:
+    lst.append(int(i))
+for i in range(len(lst)):
+    if lst[i]==6:
+        lst[i]=9
+        break;
+for i in lst:
+    print(i,end="")
