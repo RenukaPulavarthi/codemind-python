9b0878c0ee875c249d54edb34275daf9fc3a7fb8
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-09-10 08:15:54 +0530

    def vowel(s):     l=0     for i in s:         if i.isalpha() and i in "aeiouAEIOU":             l+=1     return l
    lst=list(input().split())
    l=[]
    for i in lst:
        l.append(vowel(i))
    print(max(l))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,12 @@
+def vowel(s):
+    l=0
+    for i in s:
+        if i.isalpha() and i in "aeiouAEIOU":
+            l+=1
+    return l
+
+lst=list(input().split())
+l=[]
+for i in lst:
+    l.append(vowel(i))
+print(max(l))
