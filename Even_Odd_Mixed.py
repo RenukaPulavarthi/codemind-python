d6e45f10d1bcca80464dda3da052e9fc25e3c1c6
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-10-22 16:22:11 +0530

    n=int(input()) o=0 e=0 while(n):     re=n%10     if re%2==0:         e+=1     else:         o+=1     n=n//10 if e>0 and o>0:     print("Mixed") elif e>0:     print("Even") else:     print("Odd")

--- Even_Odd_Mixed.py
+++ Even_Odd_Mixed.py
@@ -1,16 +1,16 @@
 n=int(input())
-s=n
-lst=[]
-c=0
-while n:
-    lst.append(n%10)
+o=0
+e=0
+while(n):
+    re=n%10
+    if re%2==0:
+        e+=1
+    else:
+        o+=1
     n=n//10
-for i in lst:
-    if i%2==0:
-        c+=1
-if c==len(lst):
-    print("Even")
-elif c<len(lst) and c!=0:
+if e>0 and o>0:
     print("Mixed")
+elif e>0:
+    print("Even")
 else:
     print("Odd")
