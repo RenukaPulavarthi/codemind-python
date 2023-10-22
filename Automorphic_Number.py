d5795014aa0515ddfdb654d12383c615cde9bedd
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-10-22 16:28:41 +0530

    n=int(input()) l=len(str(n)) s=(n*n)%(10**l) if s==n:     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,9 +1,7 @@
-n=input()
-nu=int(n)
-s=nu*nu
-l=len(n)
-k=s%(10**l)
-if k==nu:
+n=int(input())
+l=len(str(n))
+s=(n*n)%(10**l)
+if s==n:
     print("Automorphic Number")
 else:
     print("Not an Automorphic Number")
